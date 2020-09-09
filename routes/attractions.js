283a15607eb97312cd0287cab6477b0e0974b2c7
Connor Twele <connortwele@gmail.com>
2020-09-09 14:35:44 -0400

    commenting out attractions backend route

--- routes/attractions.js
+++ routes/attractions.js
@@ -1,13 +1,13 @@
 const router = require('express').Router()
 let Attraction = require('../models/attractions.model')
-router.route('/').get((req,res)=>{
-    Attraction.find()
-    .then(attractions=>
-        res.json(attractions))
-        .catch((err)=>{
-        res.status(400).json("Error: " + err)
-    })
-})
+// router.route('/').get((req,res)=>{
+//     Attraction.find()
+//     .then(attractions=>
+//         res.json(attractions))
+//         .catch((err)=>{
+//         res.status(400).json("Error: " + err)
+//     })
+// })
 router.route('/:id').get((req,res)=>{
     Attraction.findById(req.params.id)
     .then((exercise)=>{
