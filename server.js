ea803dd5b88a396593fe51f4535d5299d6d2767d
Connor Twele <connortwele@gmail.com>
2020-09-09 15:26:28 -0400

    deleted doubles

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000
+const path = require('path')
 app.use(cors())
 app.use(express.json())
 app.use(express.static('client/build'))
@@ -19,9 +20,12 @@ const attractionsRouter = require('./routes/attractions')
 const contactsRouter = require('./routes/contacts')
 app.use('/attractions', attractionsRouter)
 app.use('/contact', contactsRouter)
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) // relative path
+    })
+  }
 app.listen(port, ()=>{
     console.log(`server is running on port: ${port}`)
 })
