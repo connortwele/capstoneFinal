f78613f8463285ee2a83593353ea70d40455c0ee
Connor Twele <connortwele@gmail.com>
2020-09-09 14:23:38 -0400

    update server.js

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const app = express();
 const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
-app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) // relative path
+    })
+  }
 
 const uri = process.env.ATLAS_URI
 mongoose.connect(uri,{useNewUrlParser: true, useCreateIndex: true})
